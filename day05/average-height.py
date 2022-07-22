4e250a306b2cc70c3b2bc34af7d1a38b62606b7b
jman <new1726@naver.com>
2022-07-23 00:35:07 +0900

    day05, 06 completed

---
+++ day05/average-height.py
@@ -0,0 +1,16 @@
+# Don't change the code below
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
+# Don't change the code above
+
+
+# Write your code below this row
+heights_sum = 0
+
+for height in student_heights:
+    heights_sum += height
+
+heights_avg = round(heights_sum / len(student_heights))
+
+print(heights_avg)
---
+++ day05/password-generator.py
@@ -0,0 +1,38 @@
+# Password Generator Project
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+
+password = []
+
+
+# 1. pick random characters and add to the list {password}
+def password_generator(count, component_list):
+    for i in range(count):
+        password.append(component_list[random.randint(0, len(component_list) - 1)])
+        # alternative) password.append(random.choice(component_list))
+
+
+# 2. randomly shuffle the list {password} and join every character in the list
+def password_merger(pw_list):
+    random.shuffle(pw_list)
+    final_password = ''.join(pw_list)
+    print(f"Here is your password: {final_password}")
+
+
+password_generator(nr_letters, letters)
+password_generator(nr_symbols, symbols)
+password_generator(nr_numbers, numbers)
+
+
+password_merger(password)
